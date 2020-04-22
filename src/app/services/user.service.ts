37f6fd91f5901276e7f2fea3c0b76d50d62e6b9e
midoriya-izuku <sairajmayekar12@gmail.com>
2020-04-22 13:26:08 +0530

    third build

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -12,13 +12,13 @@ export class UserService {
   getUsers(userId){
     const body = JSON.stringify({'user_ids':userId});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/getUsers', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/getUsers', body, {withCredentials:true,headers: headers})
         .pipe(map((response: HttpResponse<Object>) => response));
   }
   getUsersandPosts(searchTerm) {
     const body = JSON.stringify({searchTerm:searchTerm});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/getUsersandPosts', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/getUsersandPosts', body, {withCredentials:true,headers: headers})
         .pipe(map((response: HttpResponse<Object>) => response));
         
   }
@@ -26,7 +26,7 @@ export class UserService {
   follow(user,userData){
     const body = JSON.stringify({followUser:user,user:userData});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/followUser', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/followUser', body, {withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
   
@@ -40,7 +40,7 @@ export class UserService {
   readAll(userData){
     const body = JSON.stringify({user:userData});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/readAll', body,{withCredentials:true,headers: headers})
+    return this.http.post('/user/readAll', body,{withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
 
@@ -48,14 +48,14 @@ export class UserService {
   saveOrUnSave(post_id,user_id,saveType){
     const body = JSON.stringify({'post_id':post_id,'user_id':user_id,'saveType':saveType});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/saveOrUnSave', body,{withCredentials:true,headers: headers})
+    return this.http.post('/user/saveOrUnSave', body,{withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
 
   editProfile(userData,newUserData){
     const body = JSON.stringify({'userData':userData,'newUserData':newUserData});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/editProfile', body,{withCredentials:true,headers: headers})
+    return this.http.post('/user/editProfile', body,{withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   
   }
@@ -63,21 +63,21 @@ export class UserService {
   establishChat(from_user_id,to_user_id){
     const body = JSON.stringify({from_user_id:from_user_id,to_user_id:to_user_id});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/establishChat', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/establishChat', body, {withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
 
   getMessages(from_user_id){
     const body = JSON.stringify({from_user_id:from_user_id});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/getMessages', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/getMessages', body, {withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
 
   recommendedUsers(user_id,following){
     const body = JSON.stringify({user_id:user_id,following:following});
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.post('user/recommendedUsers', body, {withCredentials:true,headers: headers})
+    return this.http.post('/user/recommendedUsers', body, {withCredentials:true,headers: headers})
     .pipe(map((response: HttpResponse<Object>) => response));
   }
 
